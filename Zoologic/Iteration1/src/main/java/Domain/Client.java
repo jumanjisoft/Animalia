1dd251d3dad4c60dc5129ef083e4ddecb65f3de3
Jaime Cepeda Villamayor <jcepedavillamayor@gmail.com>
2016-06-14 18:15:25 +0200

    Version 0.2
    Client class in the Domain added

---
+++ Zoologic/Iteration1/src/main/java/Domain/Client.java
@@ -0,0 +1,28 @@
+package Domain;
+
+public class Client {
+	private String user;
+	private String password;
+
+	public Client(String user, String password) {
+		this.user = user;
+		this.password = password;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
